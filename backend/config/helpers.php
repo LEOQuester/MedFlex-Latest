d3b0b62d992389a0fd5f814ea435a9a3f518f213
Induwara Lakindu <induwaralakindu09@gmail.com>
2025-10-15 16:41:31 +0530

    refined initial setup

--- backend/config/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Helper function for JSON responses
-function jsonResponse($response, $data, $statusCode = 200) {
-    $response->getBody()->write(json_encode($data));
-    return $response->withHeader('Content-Type', 'application/json')->withStatus($statusCode);
-}
